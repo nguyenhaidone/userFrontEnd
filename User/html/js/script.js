ebe93594b21030289a301c99ab1383eb94a3d9e7
nguyenhaidone <49014302+nguyenhaidone@users.noreply.github.com>
2019-11-06 21:41:08 +0700

    First Commit

--- User/html/js/script.js
+++ User/html/js/script.js
@@ -1,10 +1,33 @@
-window.onload = ()=>{
+window.onload = () => {
+    document.querySelector('#vid').play();
     const form = document.querySelector('#question');
 
-    form.addEventListener('submit',(event)=>{
+    form.addEventListener('submit', (event) => {
         event.preventDefault();
-        const questions = form.keyword.value
+        const questions = form.ques.value
+        const email = form.email.value
         alert('Cảm ơn câu hỏi của bạn!!!')
-        console.log(questions)
+        console.log("Email: " + email + " Question: " + questions)
     });
+    const searchBox = document.querySelector('#search');
+
+    searchBox.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const keySearch = searchBox.searchCar.value;
+
+        if (keySearch == 'mazda' || keySearch == 'Mazda 6' || keySearch == 'Mazda 2' || keySearch == 'Mazda 3' || keySearch == 'Mazda cx3' || keySearch == 'Mazda cx5' || keySearch == 'Mazda cx8') {
+            window.location.href = "./mazda.html"
+        } else {
+            if (keySearch == 'lamborghini' || keySearch == 'maserati') {
+                alert('Xin lỗi bạn, chúng tôi đang cập nhật sản phẩm này!');
+            } else {
+                alert('Xin lỗi bạn, chúng tôi không có sản phẩm này!');
+            }
+        }
+        console.log(keySearch)
+    })
+
+
+
+    document.querySelector('#vid').play();
 };
