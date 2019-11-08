66629ca6a5241c331fa3283d8b47e1e2efd58cbd
nguyenhaidone <49014302+nguyenhaidone@users.noreply.github.com>
2019-11-08 16:08:35 +0700

    commit

--- User/html/js/script.js
+++ User/html/js/script.js
@@ -4,9 +4,20 @@ window.onload = () => {
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        const questions = form.ques.value
-        const email = form.email.value
-        alert('Cảm ơn câu hỏi của bạn!!!')
+        const questions = form.ques.value;
+        const email = form.email.value;
+        const emailRegex = new RegExp(/^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/g);
+        const quesRegex = new RegExp(/^\w+$/);
+        
+        if(emailRegex.test(email)){
+            if(quesRegex.test(questions)){
+                alert("Cảm ơn bạn đã gửi câu hỏi cho chúng tôi!")
+            } else {
+                alert("Vui lòng điền câu hỏi!")
+            }
+        }else{
+            alert('Email không hợp lệ')
+        }        
         console.log("Email: " + email + " Question: " + questions)
     });
     const searchBox = document.querySelector('#search');
